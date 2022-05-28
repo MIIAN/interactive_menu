395ac731b528ede7cfe21acbecbf51350d24df01
MILAN <ninich600@gmail.com>
2022-05-27 22:32:35 -0700

    adding drop down choices for icons

--- interactiveMenu.php
+++ interactiveMenu.php
@@ -14,12 +14,10 @@ This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
 as published by the Free Software Foundation; either version 2
 of the License, or (at your option) any later version.
-
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
-
 You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
@@ -49,14 +47,15 @@ class interactive_menu extends WP_Widget {
     // Front-End
     public function widget( $args, $instance ) {
         $title = apply_filters( 'widget_title', $instance['title'] );
+        $fafaicon = apply_filters( 'widget_icon', $instance['fafaicon'] ); 
          
         echo $args['before_widget'];
-        if ( ! empty( $title ) )
+
         echo __( '
         '. do_action('interactive_menu') . '
         <div class="widget-wrap"
         <div class="menuClass icon-widget" id="menuID">
-            <i class="fa fa-arrow-circle-down fa-3x rotates" style="color:#ffffff;background-color:transparent;" id="iconID"> </i>
+            <i class="' . $instance['fafaicon'] . ' fa-3x rotates" style="color:#ffffff;background-color:transparent;" id="iconID"> </i>
         <br>
         <h4 class="widget-title">' . $title . '</h4>
         </div>
@@ -67,26 +66,55 @@ class interactive_menu extends WP_Widget {
      
     // Back-End
         public function form( $instance ) {
-        if ( isset( $instance[ 'title' ] ) ) {
-        $title = $instance[ 'title' ];
-        }
-        else {
-        $title = __( 'New title', 'wpb_widget_domain' );
-        }
-        // Widget admin form
-        ?>
+            
+            $title = ! empty( $instance['title'] ) ? $instance['title'] : esc_html__( 'title', 'yts_domain' ); 
+      
+            $fafaicon = ! empty( $instance['fafaicon'] ) ? $instance['fafaicon'] : esc_html__( 'fa-arrow-circle-down', 'yts_domain' );
+            ?>
+      <!-- TITLE -->
+      <p>
+        <label for="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>">
+          <?php esc_attr_e( 'Title:', 'yts_domain' ); ?>
+        </label> 
+
+        <input 
+          class="widefat" 
+          id="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>" 
+          name="<?php echo esc_attr( $this->get_field_name( 'title' ) ); ?>" 
+          type="text" 
+          value="<?php echo esc_attr( $title ); ?>">
+      </p>
+
+      <!-- Fa-Fa-Icon -->
         <p>
-        <label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label>
-        <input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
+            <label for="<?php echo esc_attr( $this->get_field_id( 'fafaicon' ) ); ?>">
+              <?php esc_attr_e( 'Icon:', 'yts_domain' ); ?>
+            </label> 
+
+            <select 
+                    class="widefat" 
+                    id="<?php echo esc_attr( $this->get_field_id( 'fafaicon' ) ); ?>" 
+                    name="<?php echo esc_attr( $this->get_field_name( 'fafaicon' ) ); ?>">
+                <option value="fa fa-arrow-circle-down" <?php echo ($fafaicon == 'fa fa-arrow-circle-down') ? 'selected' : ''; ?>>
+                    Arrow Down 
+                </option>
+                <option value="fa fa-gears" <?php echo ($fafaicon == 'fa fa-gears') ? 'selected' : ''; ?>>
+                    Gears 
+                </option>
+            </select>
         </p>
-        <?php
+    <?php
     }
      
     // Updating 
     public function update( $new_instance, $old_instance ) {
-        $instance = array();
-        $instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
-        return $instance;
+        $instance = array(); 
+        
+      $instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+
+      $instance['fafaicon'] = ( ! empty( $new_instance['fafaicon'] ) ) ? strip_tags( $new_instance['fafaicon'] ) : '';
+  
+      return $instance;
     }
     
     // Styles
@@ -117,7 +145,3 @@ function wpb_load_widget() {
     register_widget( 'interactive_menu' );
 }
 add_action( 'widgets_init', 'wpb_load_widget' );
-
-
-
-
